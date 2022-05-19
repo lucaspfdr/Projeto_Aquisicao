e37099f53124b19830b95c149d793d9ca225f90c
Lucas Del Rio <lucaspfdr@gmail.com>
2022-05-19 20:38:26 -0300

    Save File

--- src/renderer.js
+++ src/renderer.js
@@ -5,8 +5,14 @@
 // selectively enable features needed in the rendering
 // process.
 
+const fs = require('fs')
 const {ipcRenderer} = require('electron');
 const spawn = require('child_process').spawn
+let flag_leitura = true
+
+
+
+
 const data_grafico_1 =
 {
     type: 'line',
@@ -17,7 +23,8 @@ const data_grafico_1 =
         [
 
             {
-                label: 'Sinais 10',
+                label: 'Temperatura',
+                tension: 0.4,
                 borderColor: 'rgba(0, 120, 200, 0.5)',
                 data: []
             }
@@ -34,7 +41,8 @@ const data_grafico_2 =
         datasets:
         [
             {
-                label: 'Sinais 1',
+                label: 'Umidade',
+                tension: 0.4,
                 borderColor: 'rgba(200, 0, 200, 0.5)',
                 data: []
             }
@@ -42,43 +50,69 @@ const data_grafico_2 =
     }
 };
 
-const charts = [
-    new Chart(document.getElementById('plot-1'), data_grafico_1),
-    new Chart(document.getElementById('plot-2'), data_grafico_2)
-]
 
-let data = [
+
+
+function temp() {
+    flag_leitura = true
+    console.log(flag_leitura)
+}
+
+// Dados dos sensores
+// TODO Apenas gerar os graficos depois de clickar no botao
+
+
+if (flag_leitura === true) {
+
+    let data = [
+        {
+            x:0,
+            y:0,
+            max: 10,
+            min: -10
+        },
+        {
+            x:0,
+            y:0,
+            max: 1,
+            min: -1
+        }
+    ]
+    
+    
+    
+    
+    setInterval( ()=>
     {
-        x:0,
-        y:0,
-        max: 10,
-        min: -10
-    },
-    {
-        x:0,
-        y:0,
-        max: 1,
-        min: -1
-    }
-]
-
-setInterval( ()=>
-{
-    for (let i=0; i< 2; i++){
-        data[i].y= 2 * Math.random() * data[i].max + data[i].min;
-        charts[i].data.labels.push(data[i].x++);
-        charts[i].data.datasets[0].data.push(data[i].y);
-        charts[i].update()
-    }
-}, 
-500);
-
-
+        for (let i=0; i< 2; i++){
+            data[i].y= 2 * Math.random() * data[i].max + data[i].min;
+            charts[i].data.labels.push(data[i].x++);
+            charts[i].data.datasets[0].data.push(data[i].y);
+            charts[i].update()
+        }
+    }, 
+    500);
+}
+    
+    
+    
 function exit(){
     console.log('entrei')
     ipcRenderer.send('exit')
 }
 
 function help(){
- spawn('powershell',['Start-Process', 'https://github.com/lucaspfdr/Projeto_Aquisicao'])
+    spawn('powershell',['Start-Process', 'https://github.com/lucaspfdr/Projeto_Aquisicao'])
 }
+
+
+// Para salvar depois do click fs.appendFileSync(...)
+function saveTemp() {
+    fs.appendFileSync('Dados_1.csv', data_grafico_1.data.datasets[0].data + ";" + new Date().toLocaleString());
+}
+
+
+const charts = [
+    new Chart(document.getElementById('plot-1'), data_grafico_1),
+    new Chart(document.getElementById('plot-2'), data_grafico_2)
+]
